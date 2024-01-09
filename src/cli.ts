24f70a4dbec434bf10446dd46cc4a50cc1353896
csskr <csskr@qq.com>
2024-01-09 16:12:46 +0800

    feat(network): add display of local network addresses
    Adds the display of local network addresses when starting the server.
    
    BREAKING CHANGE: none

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,32 @@
 #!/usr/bin/env node
-import { join } from "path"
-import { ANSIColor, color } from "tutils/ansi"
-import { CommandLineOption, formatCommandLineOptions, parseCommandLineArguments } from "tutils/commandLine"
-import { HTTPRequest, HTTPResponse } from "tutils/httpServer"
-import { Matcher } from "tutils/matcher"
-import { WebServer } from "./webServer"
+import { join } from "path";
+import { ANSIColor, color } from "tutils/ansi";
+import {
+	CommandLineOption,
+	formatCommandLineOptions,
+	parseCommandLineArguments,
+} from "tutils/commandLine";
+import { HTTPRequest, HTTPResponse } from "tutils/httpServer";
+import { Matcher } from "tutils/matcher";
+import { WebServer } from "./webServer";
+import os from "os";
 
 const commandOptions: { [option: string]: CommandLineOption } = {
 	// Host
 	"--host": {
 		group: "Host",
 		argument: "host",
-		description: "Serve Address to use [0.0.0.0]:[auto]"
+		description: "Serve Address to use [0.0.0.0]:[auto]",
 	},
 	"--port": {
 		alias: ["-p"],
 		argument: "port",
-		description: "Port to use [auto]"
+		description: "Port to use [auto]",
 	},
 	"--address": {
 		alias: ["-a"],
 		argument: "address",
-		description: "Address to use [0.0.0.0]"
+		description: "Address to use [0.0.0.0]",
 	},
 
 	// Browser
@@ -30,121 +35,139 @@ const commandOptions: { [option: string]: CommandLineOption } = {
 		alias: ["-o"],
 		argument: "path",
 		default: true,
-		description: `Open browser window after starting the server\nOptionally provide a URL path to open the browser window to`
+		description: `Open browser window after starting the server\nOptionally provide a URL path to open the browser window to`,
 	},
 	"--open-url": {
 		argument: "path",
 		default: "",
-		description: `provide a URL path to open the browser window to`
+		description: `provide a URL path to open the browser window to`,
 	},
 	"--open-client": {
 		argument: "app",
 		default: "",
-		description: `default browser`
+		description: `default browser`,
 	},
 	"--no-dir": {
-		description: "Do not show directory list"
+		description: "Do not show directory list",
 	},
 
 	// Logger
 	"--utc": {
 		group: "Logger",
 		alias: ["-U"],
-		description: "Use UTC time format in log messages"
+		description: "Use UTC time format in log messages",
 	},
 	"--log": {
 		alias: ["-s"],
-		description: "Print log messages"
+		description: "Print log messages",
 	},
 	"--log-ip": {
-		description: "Enable logging of the client\"s IP address"
+		description: 'Enable logging of the client"s IP address',
 	},
 
 	// Server
 	"--cwd": {
 		group: "server",
 		argument: "path",
-		description: "cwd"
+		description: "cwd",
 	},
 	"--proxy": {
 		alias: ["-P"],
 		argument: "url",
-		description: "Fallback proxy if the request cannot be resolved. e.g.: http://github.com"
+		description:
+			"Fallback proxy if the request cannot be resolved. e.g.: http://github.com",
 	},
 
 	"--root-path": {
 		argument: "path",
-		description: ""
+		description: "",
 	},
 	"--index": {
 		alias: ["-I"],
 		multiple: true,
 		argument: "fileName",
-		description: "main file"
+		description: "main file",
 	},
 	"--ssl": {
-		description: "Enable https"
+		description: "Enable https",
 	},
 	"--cert": {
 		argument: "filePath",
 		default: "",
-		description: "Path to ssl cert file (default: cert.pem)."
+		description: "Path to ssl cert file (default: cert.pem).",
 	},
 	"--key": {
 		argument: "filePath",
 		default: "",
-		description: "Path to ssl key file (default: key.pem)"
+		description: "Path to ssl key file (default: key.pem)",
 	},
 	"--http2": {
-		description: "Enable http2"
+		description: "Enable http2",
 	},
 	"--max-length": {
 		argument: "maxAllowedContentLength",
-		description: "max allowed content length (default: 20 * 1024 * 1024)"
+		description: "max allowed content length (default: 20 * 1024 * 1024)",
 	},
 	"--version": {
 		alias: ["-v"],
-		description: "Print version"
+		description: "Print version",
 	},
 	"--help": {
 		alias: ["-h", "-?"],
-		description: "Print help"
-	}
-}
-const argv = parseCommandLineArguments(commandOptions)
+		description: "Print help",
+	},
+};
+const argv = parseCommandLineArguments(commandOptions);
 if (argv["--help"]) {
-	console.info([
-		"Usage: h2server [port or url] [options]",
-		"",
-		"Options:",
-		formatCommandLineOptions(commandOptions)
-	].join("\n"))
-	process.exit()
+	console.info(
+		[
+			"Usage: h2server [port or url] [options]",
+			"",
+			"Options:",
+			formatCommandLineOptions(commandOptions),
+		].join("\n")
+	);
+	process.exit();
 }
 
-const port = argv["--port"] || ""
-const address = argv["--address"] || ""
-const rootServe = argv["--root-path"] || "/"
-const rootDir = argv["--cwd"] as string || process.cwd()
-const openURL = argv["--open-url"] as string
-const open = argv["--open"] as string | boolean
-const url = typeof open === "string" ? open : argv[0] as string || argv["--host"] as string || `${address}${port ? `:${port}` : ""}${rootServe}`
+const port = argv["--port"] || "";
+const address = argv["--address"] || "";
+const rootServe = argv["--root-path"] || "/";
+const rootDir = (argv["--cwd"] as string) || process.cwd();
+const openURL = argv["--open-url"] as string;
+const open = argv["--open"] as string | boolean;
+const url =
+	typeof open === "string"
+		? open
+		: (argv[0] as string) ||
+		  (argv["--host"] as string) ||
+		  `${address}${port ? `:${port}` : ""}${rootServe}`;
 
 const logger = {
-	log: argv["--log"] ? console.info : () => { },
-	request: (request: HTTPRequest, response: HTTPResponse, server: WebServer) => {
-		const date = argv["--utc"] ? new Date().toUTCString() : new Date()
-		const ip = argv["--log-ip"] ? request.headers["x-forwarded-for"] || "" + request.connection.remoteAddress : ""
+	log: argv["--log"] ? console.info : () => {},
+	request: (
+		request: HTTPRequest,
+		response: HTTPResponse,
+		server: WebServer
+	) => {
+		const date = argv["--utc"] ? new Date().toUTCString() : new Date();
+		const ip = argv["--log-ip"]
+			? request.headers["x-forwarded-for"] ||
+			  "" + request.connection.remoteAddress
+			: "";
 		logger.log(
 			"[%s] %s %s %s %s",
-			date, ip, color(request.method, ANSIColor.cyan), color(request.url, ANSIColor.cyan),
+			date,
+			ip,
+			color(request.method, ANSIColor.cyan),
+			color(request.url, ANSIColor.cyan),
 			request.headers["user-agent"]
-		)
-	}
-}
-const staticPath = argv["--static"] as string | undefined
-const redirect = argv["--redirect"] as string | undefined
-const proxy = argv["--proxy"] as string | undefined
+		);
+	},
+};
+const staticPath = argv["--static"] as string | undefined;
+const redirect = argv["--redirect"] as string | undefined;
+const proxy = argv["--proxy"] as string | undefined;
 
 const server = new WebServer({
 	url,
@@ -152,7 +175,7 @@ const server = new WebServer({
 	http2: !!argv["--http2"] || !!(url && /^https:/i.test(url)),
 	cert: argv["--cert"] as string,
 	key: argv["--key"] as string,
-	open: argv["--open-client"] as string || !!open || openURL,
+	open: (argv["--open-client"] as string) || !!open || openURL,
 	openURL,
 	rootDir,
 	routers: [
@@ -163,60 +186,87 @@ const server = new WebServer({
 			rewrite: redirect,
 			break: !!redirect || !!proxy || false,
 			async process(req, res, server) {
-				const { path } = req
-				const { fs } = server
-				const filePath = server.mapPath(path)
-				const fileStaticPath = server.mapPath(join(staticPath || "", path))
-				const matcher = new Matcher("**" || (() => true), undefined, true)
+				const { path } = req;
+				const { fs } = server;
+				const filePath = server.mapPath(path);
+				const fileStaticPath = server.mapPath(join(staticPath || "", path));
+				const matcher = new Matcher("**" || (() => true), undefined, true);
 				if (staticPath && filePath) {
 					if (typeof staticPath === "string") {
 						try {
-							const stat = await fs.getStat(filePath)
+							const stat = await fs.getStat(filePath);
 							if (stat.isDirectory() && fileStaticPath) {
-								const entries = await fs.readDir(filePath)
-								const entriesStatic = await fs.existsDir(fileStaticPath) ? await fs.readDir(fileStaticPath) : []
+								const entries = await fs.readDir(filePath);
+								const entriesStatic = (await fs.existsDir(fileStaticPath))
+									? await fs.readDir(fileStaticPath)
+									: [];
 								for (const index of server.defaultPages) {
-									if (!entries.includes(index) && entriesStatic.includes(index)) {
-										const indexPath = join(fileStaticPath, index)
+									if (
+										!entries.includes(index) &&
+										entriesStatic.includes(index)
+									) {
+										const indexPath = join(fileStaticPath, index);
 										if (await fs.existsFile(indexPath)) {
-											req.path = join(staticPath, path, index)
+											req.path = join(staticPath, path, index);
 										}
 									}
 								}
 							}
-						}
-						catch (e) {
+						} catch (e) {
 							if (fileStaticPath) {
-								const stat = await fs.getStat(fileStaticPath)
+								const stat = await fs.getStat(fileStaticPath);
 								if (stat.isFile()) {
-									req.path = join(staticPath, path)
+									req.path = join(staticPath, path);
 								}
 							}
 						}
 					}
 				}
-				logger.request(req, res, server)
-			}
+				logger.request(req, res, server);
+			},
 		},
 		{
 			match: "*.ejs",
 			async process(req, res, server) {
-				await server.writeEJS(req, res, server.mapPath(req.path)!)
-			}
+				await server.writeEJS(req, res, server.mapPath(req.path)!);
+			},
 		},
 		{
 			match: "*.njs",
 			async process(req, res, server) {
-				await server.writeServerJS(req, res, server.mapPath(req.path)!)
-			}
-		}
+				await server.writeServerJS(req, res, server.mapPath(req.path)!);
+			},
+		},
 	],
 	defaultPages: argv["--index"] as string[],
 	directoryList: !argv["--no-dir"],
-	maxAllowedContentLength: typeof argv["--max-length"] === "string" ? parseInt(argv["--max-length"], 10) : undefined
-})
+	maxAllowedContentLength:
+		typeof argv["--max-length"] === "string"
+			? parseInt(argv["--max-length"], 10)
+			: undefined,
+});
 
 server.start().then(() => {
-	console.info(color(`Server Running At ${server.url}`, ANSIColor.green))
-	console.info("Hit CTRL-C to stop the server")
-})
+	// 获取本机所有网络接口的信息
+	const networkInterfaces = os.networkInterfaces();
+	// 创建一个数组，用于存储本地网络的IP地址
+	const networkAddresses = [
+		`Local:   ${server.url}`, // 本地地址
+	];
+
+	// 提取IPv4地址并添加到数组
+	for (const interfaceName in networkInterfaces) {
+		const interfaces = networkInterfaces[interfaceName];
+		for (const iface of interfaces) {
+			if (iface.family === "IPv4" && !iface.internal) {
+				// 去掉CIDR后缀 /32
+				const ipAddress = iface.address.replace(/\/32$/, "");
+				networkAddresses.push(`Network: http://${ipAddress}:${server.port}`);
+			}
+		}
+	}
+
+	// 在控制台中展示所有本地网络的IP地址
+	console.info(networkAddresses.join("\n"), ANSIColor.green);
+	console.info("Hit CTRL-C to stop the server");
+});
